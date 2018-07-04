a400c796c8ac4a05c49e8c45fb93549e10ee2c0e
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2018-07-04 04:38:51 -0700

    fix: iOS scanning

--- app/js/main.js
+++ app/js/main.js
@@ -3,113 +3,107 @@ import {snackbar} from './snackbar.js';
 import styles from '../css/styles.css';
 import isURL from 'is-url';
 
-//If service worker is installed, show offline usage notification
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.ready.then((registration) => {
-    if (!localStorage.getItem("offline")) {
-      localStorage.setItem("offline", true);
-      snackbar.show('App is ready for offline usage.', 5000);
-    }
-  });
+main();
+function main() {
+  setupServiceWorker();
+  window.addEventListener("DOMContentLoaded", onDOMContentLoad);
 }
 
-//To generate sw.js file
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install();
+function setupServiceWorker() {
+  // If service worker is installed, show offline usage notification
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      if (!localStorage.getItem("offline")) {
+        localStorage.setItem("offline", true);
+        snackbar.show('App is ready for offline usage.', 5000);
+      }
+    });
+  }
+
+  //To generate sw.js file
+  if (process.env.NODE_ENV === 'production') {
+    require('offline-plugin/runtime').install();
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  //To check the device and add iOS support
-  window.iOS = ['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) >= 0;
-
-  var copiedText = null;
-  var frame = null;
-  var selectPhotoBtn = document.querySelector('.app__select-photos');
-  var dialogElement = document.querySelector('.app__dialog');
-  var dialogOverlayElement = document.querySelector('.app__dialog-overlay');
-  var dialogOpenBtnElement = document.querySelector('.app__dialog-open');
-  var dialogCloseBtnElement = document.querySelector('.app__dialog-close');
-  var scanningEle = document.querySelector('.custom-scanner');
-  var textBoxEle = document.querySelector('#result');
-  var helpText = document.querySelector('.app__help-text');
-  var infoSvg = document.querySelector('.app__header-icon svg');
-  var videoElement = document.querySelector('video');
+function onDOMContentLoad() {
+  let copiedText;
+  let frame;
+  const selectPhotoButton = document.querySelector('.app__select-photos');
+  const dialogElement = document.querySelector('.app__dialog');
+  const dialogOverlayElement = document.querySelector('.app__dialog-overlay');
+  const dialogOpenButton = document.querySelector('.app__dialog-open');
+  const dialogCloseButton = document.querySelector('.app__dialog-close');
+  const scanner = document.querySelector('.custom-scanner');
+  const textBox = document.querySelector('#result');
+  const helpText = document.querySelector('.app__help-text');
+  const infoSvg = document.querySelector('.app__header-icon svg');
+  const videoElement = document.querySelector('video');
   window.appOverlay = document.querySelector('.app__overlay');
     
-  //Initializing qr scanner
+  // Setup event listeners
   window.addEventListener('load', (event) => {
-    QRReader.init(); //To initialize QR Scanner
+    QRReader.init("#cam");
+
     // Set camera overlay size
     setTimeout(() => { 
       setCameraOverlay();
-      if (!window.iOS) {
-        scan();
-      }
+      scan();
     }, 1000);
   });
+  dialogCloseButton.addEventListener('click', hideDialog, false);
+  dialogOpenButton.addEventListener('click', openInBrowser, false);
 
   function setCameraOverlay() {
     window.appOverlay.style.borderStyle = 'solid';
     helpText.style.display = 'block';
   }
-  
-  function createFrame() {
-    frame = document.createElement('img');
-    frame.src = '';
-    frame.id = 'frame';
-  }
-  
-  //Dialog close btn event
-  dialogCloseBtnElement.addEventListener('click', hideDialog, false);
-  dialogOpenBtnElement.addEventListener('click', openInBrowser, false);
 
   //To open result in browser
   function openInBrowser() {
-    console.log('Result: ', copiedText);
     window.open(copiedText, '_blank', 'toolbar=0,location=0,menubar=0');
     copiedText = null;
     hideDialog();
   }
 
-  //Scan
   function scan() {
-    if (!window.iOS) scanningEle.style.display = 'block';
-    QRReader.scan((result) => {
+    scanner.style.display = 'block';
+
+    QRReader.scan(result => {
       copiedText = result;
-      textBoxEle.value = result;
-      textBoxEle.select();
-      scanningEle.style.display = 'none';
+      textBox.value = result;
+      textBox.select();
+      scanner.style.display = 'none';
       if (isURL(result)) {
-        dialogOpenBtnElement.style.display = 'inline-block';
+        dialogOpenButton.style.display = 'inline-block';
       }
       dialogElement.classList.remove('app__dialog--hide');
       dialogOverlayElement.classList.remove('app__dialog--hide');
     });
   }
 
-  //Hide dialog
   function hideDialog() {
     copiedText = null;
-    textBoxEle.value = "";
-
-    if (window.iOS) {
-      frame.src = "";
-      frame.className = "";
-    }
+    textBox.value = "";
 
     dialogElement.classList.add('app__dialog--hide');
     dialogOverlayElement.classList.add('app__dialog--hide');
     scan();
   }
 
-  // For iOS support
-  if (window.iOS) selectFromPhoto();
+  function createFrame() {
+    frame = document.createElement('img');
+    frame.src = '';
+    frame.id = 'frame';
+  }
 
   function selectFromPhoto() {
-    if (videoElement) videoElement.remove(); //removing the video element
+    if (videoElement) {
+      videoElement.remove();
+    }
     
-    //Creating the camera element
-    var camera = document.createElement('input');
+    // Creating the camera element
+    const camera = document.createElement('input');
     camera.setAttribute('type', 'file');
     camera.setAttribute('capture', 'camera');
     camera.id = 'camera';
@@ -118,30 +112,30 @@ window.addEventListener("DOMContentLoaded", () => {
     helpText.style.bottom = '-60px';
     infoSvg.style.fill = '#212121';
     window.appOverlay.style.borderStyle = '';
-    selectPhotoBtn.style.color = "#212121";
-    selectPhotoBtn.style.display = 'block';
+    selectPhotoButton.style.color = "#212121";
+    selectPhotoButton.style.display = 'block';
     createFrame();
 
-    //Add the camera and img element to DOM
+    // Add the camera and img element to DOM
     var pageContentElement = document.querySelector('.app__layout-content');
     pageContentElement.appendChild(camera);
     pageContentElement.appendChild(frame);
 
-    //Click of camera fab icon
-    selectPhotoBtn.addEventListener('click', () => {
-      scanningEle.style.display = 'none';
+    // Click of camera fab icon
+    selectPhotoButton.addEventListener('click', () => {
+      scanner.style.display = 'none';
       document.querySelector("#camera").click();
     });
     
-    //On camera change
+    // On camera change
     camera.addEventListener('change', (event) => {
       if (event.target && event.target.files.length > 0) {
         frame.className = 'app__overlay';
         frame.src = URL.createObjectURL(event.target.files[0]);
-        scanningEle.style.display = 'block';
+        scanner.style.display = 'block';
         window.appOverlay.style.borderColor = '#212121';
         scan();
       }
     });
   }
-});
+}
